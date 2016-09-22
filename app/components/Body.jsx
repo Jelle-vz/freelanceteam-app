a4afd2adc8286975d5f735922e745a83d28d46ef
Jelle Verzijden <jelle@macaw.nl>
2016-09-22 21:59:06 +0200

    ADDED jQuery http request function + Example call to Joblist

--- app/components/Body.jsx
+++ app/components/Body.jsx
@@ -2,53 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import $ from 'jquery';
+// import { request } from 'wvz-http-request';
 
 import { Button } from 'react-bootstrap';
 
 
+function request(scope, url, type) {
+	$.ajax({
+		 url: url,
+		 dataType: type,
+		 cache: false,
+		 success: function(data) {
+			 scope.setState({data: data});
+		 }.bind(this),
+		 error: function(xhr, status, err) {
+			 console.error(url, status, err.toString());
+		 }.bind(this)
+	 });
+}
+
 export default class Jobs extends React.Component {
 	render(){
+		var jobs = this.props.data;
 		return(
-			<span>spannetje</span>
+			<ul>
+				{jobs.map(function(item){
+					return (
+						<li>{item.title} </li>
+					)
+				})}
+			</ul>
 		)
 	}
 }
 
 class Body extends React.Component {
-
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			data:[]
+			data : []
 		};
+		request(this, this.props.xhr, 'json');
+
 	}
 
-	componentDidMount() {
-		$.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
+	componentWillUnmount() {
+		request().abort();
 	}
-
 
   render() {
-		console.log(this.state)
+		let { state, props } = this;
+		let jobs = state.data.jobs;
+
     return (
 			<div>
 			 This is the body or the content.<br/>
+			 {jobs ? <Jobs data={jobs} /> : <span> Loading... </span>}
 			 <Button>Added Bootstrap support</Button>
-			 <Jobs data={this.state.data} />
 			</div>
 		)
   }
 }
 
-ReactDOM.render(<Body url="http://codepen.io/jobs.json"/>, document.getElementById('content'));
+ReactDOM.render(<Body xhr="http://codepen.io/jobs.json"/>, document.getElementById('content'));
