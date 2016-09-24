879802ecea7133ac3cc68dfcbd64bb48b8feb702
Jelle Verzijden <jelle@webverzorgers.nl>
2016-09-24 11:38:16 +0200

    ADD CSS modules support for SASS

--- app/01-atoms/tiles/Default.jsx
+++ app/01-atoms/tiles/Default.jsx
@@ -9,7 +9,7 @@ export default class Tile extends React.Component {
   render() {
     let data = this.props.data;
     return (
-      <Col xs={12} sm={4} md={3} className="user-tile">
+      <Col xs={12} sm={4} md={3} className={styles.user_tile}>
         <Thumbnail src="http://placehold.it/350x200" alt="242x200" className="Whooptydoo">
           <h3>{data.name}</h3>
           <p>{data.company.catchPhrase}</p>
