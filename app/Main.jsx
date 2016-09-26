c4729799330d21ce46cede493222fe281d951601
Jelle Verzijden <jelle@macaw.nl>
2016-09-26 16:23:02 +0200

    REFACTOR: Moved Body.jsx outside of scaffolds and renamed to Main.jsx

---
+++ app/Main.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './03-scaffolds/Header.jsx';
+import Userlist from './02-modules/user-list/Userlist.jsx';
+import Footer from './03-scaffolds/Footer.jsx';
+
+
+class Main extends React.Component {
+
+  render() {
+		let users = "https://jsonplaceholder.typicode.com/users";
+
+    return (
+			<div>
+				<Header/>
+				<h3>Example list</h3>
+			 	<Userlist data={users} />
+			 	<Footer/>
+			</div>
+		)
+  }
+}
+
+ReactDOM.render(<Main/>, document.getElementById('main'));
--- index.js
+++ index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 
 import Bootstrap from './node_modules/bootstrap/dist/css/bootstrap.css';
 
-import Header from './app/03-scaffolds/Header.jsx';
-import Body from './app/03-scaffolds/Body.jsx';
-import Footer from './app/03-scaffolds/Footer.jsx';
+import Main from './app/Main.jsx';
